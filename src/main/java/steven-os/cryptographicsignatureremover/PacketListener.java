5a69f35548fe7899f64ebfe594ab1ecec6fe34dc
Steven-OS <61627084+Steven-OS@users.noreply.github.com>
2023-07-06 22:08:47 +0200

    Update PacketListener.java
    Added the ListenerPriority.NORMAL parameter in the constructor to explicitly set the listener priority. Simplified the if statement condition by using == instead of equals() to compare PacketType. Adjusted the indentation and formatting to match the provided style. Other minor fixes.

--- src/main/java/steven-os/cryptographicsignatureremover/PacketListener.java
+++ src/main/java/steven-os/cryptographicsignatureremover/PacketListener.java
@@ -1,29 +1,28 @@
-package steven-os.cryptogragicsignatureremover;
+package steven.os.cryptographicsignatureremover;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.PacketContainer;
 import org.bukkit.plugin.Plugin;
 
 /**
- *
  * Made by Steven-OS
- * https://github.com/Steven-OS
- *
+ * GitHub: https://github.com/Steven-OS
  */
 public class PacketListener extends PacketAdapter {
+
     public PacketListener(Plugin plugin) {
-        super(plugin, PacketType.Play.Client.CHAT);
+        super(plugin, ListenerPriority.NORMAL, PacketType.Play.Client.CHAT);
     }
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
-        //Documentation: https://wiki.vg/Protocol#Chat_Message_.28serverbound.29
+        // Documentation: https://wiki.vg/Protocol#Chat_Message_.28serverbound.29
         PacketContainer packet = event.getPacket();
-        PacketType type = packet.getType();
-
-        if (!type.equals(PacketType.Play.Client.CHAT)) return;
-        packet.getBooleans().write(0, false);
+        if (packet.getType() == PacketType.Play.Client.CHAT) {
+            packet.getBooleans().write(0, false);
+        }
     }
 }
